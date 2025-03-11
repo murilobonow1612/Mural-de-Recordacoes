ecde0e58e508bb7bfc0d7d2a02e0d58a1324f10b
Murilo <murilobonow07@gmail.com>
2025-03-11 11:22:28 -0300

    refactor: improved image import method for better maintainability and best practices

---
+++ frontend/src/assets/images.ts
@@ -0,0 +1,25 @@
+import carta from './pictures/carta.png';
+import primeiroOi from './pictures/primeiroOi.png';
+import table from './pictures/t.png';
+import corredor from './pictures/corredor.png';
+import firstDate from './pictures/1_encontro.png';
+import firstDate2 from './pictures/1_encontro2.jpg';
+import pedido1 from './pictures/pedido1.png';
+import pedido2 from './pictures/pedido2.png';
+import pedido3 from './pictures/pedido3.png';
+import desdePedido from './pictures/desdePedido.png';
+
+const images = {
+  carta,
+  primeiroOi,
+  table,
+  corredor,
+  firstDate,
+  firstDate2,
+  pedido1,
+  pedido2,
+  pedido3,
+  desdePedido,
+};
+
+export default images;
